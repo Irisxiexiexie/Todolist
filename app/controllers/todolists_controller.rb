03c6882ee10bdd6c1a0b159f0e8cdce2c533c11d
Irisxiexiexie <18392885186@163.com>
2015-04-16 23:15:45 +0800

    Some helpful message for your future self

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,11 +1,16 @@
 class TodolistsController < ApplicationController
 	def index 
 		@todolist = Todolist.all 
+		 if params[:status] == "T" 
+		 	@todolist= Todolist.where("status  like?","T") 
+		 elsif params[:status] == "F" 
+		 	@todolist = Todolist.where("status like ?","F") 
+		 end
 	end 
 	def create 
 		@todolist = Todolist.new(todolist_params) 
 		if @todolist.save 
-			redirect_to root_path 
+			redirect_to root_path
 		else
 		 render :new 
 		end
@@ -26,6 +31,17 @@ class TodolistsController < ApplicationController
 		@todolist.destroy 
 		redirect_to root_path 
 	end 
+	def change 
+		@todolist = Todolist.find(params[:id])
+		 if @todolist.status == "T"
+		  	@todolist.update_attribute(:status,"F") 
+		else 
+			@todolist.update_attribute(:status,"T") 
+		end
+		 redirect_to root_path 
+	end
+
+
 	private 
 	def todolist_params
 	 params.require(:todolist).permit(:thing,:status)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'todolists#index'
 
-   resources :todolists
+   resources :todolists do
+    member do
+      post 'change'
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
